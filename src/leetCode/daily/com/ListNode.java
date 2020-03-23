80da483a1290b3765a044012120fbdb87ed28158
GMell8230 <823074582@qq.com>
2020-03-23 23:44:51 +0800

    tag:链表\双指针

---
+++ src/leetCode/daily/com/ListNode.java
@@ -0,0 +1,18 @@
+/**
+　 * Title: ListNode.java
+　 * Description:
+　 * @author GuoMell
+　 * @date 2020年2月17日下午11:07:53
+*/
+package leetCode.daily.com;
+
+public class ListNode {
+	public int val;
+	public ListNode next;
+	public ListNode(int x) {
+		val = x;
+	}
+	public ListNode() {
+		// TODO Auto-generated constructor stub
+	}
+}
