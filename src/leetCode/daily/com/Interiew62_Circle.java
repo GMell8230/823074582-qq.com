e3e8f027f8382063e8e121126c9007f9eeb214a9
GMell8230 <823074582@qq.com>
2020-03-31 23:54:00 +0800

    Tag：乌斯环

--- src/leetCode/daily/com/Interiew62_Circle.java
+++ src/leetCode/daily/com/Interiew62_Circle.java
@@ -18,12 +18,12 @@ public class Interiew62_Circle {
 	/*
 	* 递归解法
 	*/
-    public int lastRemaining(int n, int m) {
+    public static int lastRemaining(int n, int m) {
     	int res = sol(n, m);
     	return res;
     }
 
-	private int sol(int n, int m) {
+	private static int sol(int n, int m) {
 		if (n == 1) {
 			return 0;
 		}
@@ -35,7 +35,11 @@ public class Interiew62_Circle {
 	/*
 	 * 链表解法
 	 */
-	public int lastRemaining1(int n, int m) {
+	public static void main(String[] args) {
+		System.out.println(lastRemaining2(10, 3));
+	}
+	
+	public static int lastRemaining1(int n, int m) {
     	List<Integer> arr = new ArrayList<Integer>();
     	for (int i = 0; i < n; i++) {
 			arr.add(i);
@@ -48,4 +52,33 @@ public class Interiew62_Circle {
 		}
     	return arr.get(0);
     }
+	
+	  public static int lastRemaining2(int n, int m) {
+	    	int []circle = new int [n];
+	    	for (int i = 0; i < circle.length; i++) {
+				circle[i] = i;
+			}
+	    	int remain = n;
+	    	int pos = -1;
+	    	int step = 0;
+	    	while (remain > 1) {
+	    		pos = (pos+1) % n;
+	    		if (circle[pos] != -1) {
+	    			step++;
+				}
+	    		if (step == m) {
+					circle[pos] = -1;
+					System.out.println(pos);
+					remain --;
+					step = 0;
+				}
+			}
+	    	for (int i = 0; i < circle.length; i++) {
+				if (circle[i] != -1) {
+					return circle[i];
+				}
+			}
+	    	return -1;
+	    }
+	
 }
