afba799b74d639293aa42ffc1089be204c6147bf
GMell8230 <823074582@qq.com>
2020-04-08 23:50:34 +0800

    Tag：微众银行春招-03二重排序-pair结构

---
+++ src/nowcoder/springRecur/com/Pair.java
@@ -0,0 +1,16 @@
+/**
+　 * Title: Pair.java
+　 * Description:
+　 * @author GuoMell
+　 * @date 2020年4月8日下午10:11:12
+*/
+package nowcoder.springRecur.com;
+
+public class Pair {
+    Integer money;
+    Integer num;
+    public Pair(Integer money, Integer num) {
+        this.money = money;
+        this.num = num;
+    }
+}
